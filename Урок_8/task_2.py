e8fa20a5f5e0cbef703db95ba1d6753bb9887c4a
anton krivolapov <akrivola@ya.ru>
2022-12-21 16:56:13 +0300

    ДЗ урок 8 (задание 2)

--- Урок_8/task_2.py
+++ Урок_8/task_2.py
@@ -102,24 +102,30 @@ class Cell:
         self.quantity = int(quantity)
 
     def __str__(self):
-        return f'Результат операции {self.quantity * "*"}'
+        return f'Результат операции {self.quantity * "*"} ({self.quantity})'
 
     def __add__(self, other):
         return Cell(self.quantity + other.quantity)
 
     def __sub__(self, other):
-        '''
-        Выдает ошибку о том, что результат не число  при вычислении
-        if int(Cell(self.quantity - other.quantity)) > 0:
+        if self.quantity - other.quantity > 0:
             return Cell(int(self.quantity - other.quantity))
         else:
-            return f'Операция вычитания невозможна'""
-        '''
-        return self.quantity - other.quantity if (self.quantity - other.quantity) > 0 else print('Отрицательно!')
+            return 'Операция вычитания невозможна'
+    def __mul__(self, other):
+        return Cell(int(self.quantity * other.quantity))
 
-        # return Cell(int(self.quantity - other.quantity))
+    def __truediv__(self, other):
+        return Cell(self.quantity // other.quantity)
 
+    def make_order(self, cells_in_row):
+        row = ''
+        for i in range(int(self.quantity / cells_in_row)):
+            row += f'{"*" * cells_in_row} \\n'
+        row += f'{"*" * (self.quantity % cells_in_row)}'
+        return row
 
+print("Создаем объекты клеток")
 cell1 = Cell(30)
 cell2 = Cell(25)
 
