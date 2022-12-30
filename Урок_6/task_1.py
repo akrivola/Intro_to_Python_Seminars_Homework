195096976284a54117a703d41d81ff304dea59a8
anton krivolapov <akrivola@ya.ru>
2022-12-30 15:10:39 +0300

    ДЗ урок 6

--- Урок_6/task_1.py
+++ Урок_6/task_1.py
@@ -4,68 +4,19 @@
 и чего удалось добиться
 """
 from timeit import timeit
+from Урок_2.base_task_2 import switch
 
-'''
-Из урока 1 задание 3:
-Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например,
-пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-'''
+from numpy import array
 
+def improved_switch(countries):
+    for index, value in enumerate(countries):
+        if index % 2:
+            countries[index], countries[index - 1] = countries[index - 1], countries[index]
+    return countries
 
-def my_func(n):
-    sum_digits = 0
-    add = 0
-    i = 0
-    while i < n:
-        add += n * 10 ** i
-        sum_digits += add
-        i += 1
-    return sum_digits
+# switch_mem([i for i in range(100000)])
+# improved_switch(array([i for i in range(10000)]))
 
 
-def my_func2(n):
-    sum_digits = 0
-    i = n
-    ten = 1
-    while i > 0:
-        sum_digits += n * ten * i
-        ten *= 10
-        i -= 1
-    return sum_digits
+print(timeit('switch([i for i in range(100000)])', setup='from __main__ import switch', number=1000))
 
-
-a = 4
-print(timeit('my_func(a)', setup='from __main__ import my_func, a'))
-print(timeit('my_func2(a)', setup='from __main__ import my_func2, a'))
-
-'''
-Заменив возведение в степень на умножение удалось добиться небольшого прироста в скорости
-1.1145545141771436
-1.0619966173544526
-
-'''
-
-"""
-Из урока 3 задание 3:
-Реализовать функцию my_func(), которая принимает три позиционных аргумента,
-и возвращает сумму наибольших двух аргументов.
-"""
-
-
-def my_func3(arg1, arg2, arg3):
-    sorted_list = sorted([arg1, arg2, arg3])
-    return sorted_list[1] + sorted_list[2]
-
-
-def my_func4(arg1, arg2, arg3):
-    return arg1 + arg2 + arg3 - min(arg1, arg2, arg3)
-
-
-print(timeit('my_func3(14, 5, 7)', setup='from __main__ import my_func3'))
-print(timeit('my_func4(14, 5, 7)', setup='from __main__ import my_func4'))
-'''
-Отказался от использования списка и посчитал сумму 2-х наибольших аргумента как
-сумму всех минус минимальный аргумент из трех. Удалось добиться небольшого прироста в скорости
-0.41349558904767036
-0.3825996797531843
-'''
