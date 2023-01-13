e6e5b25a77cc75dceebbcb1622559ac1c046c421
anton krivolapov <akrivola@ya.ru>
2023-01-13 17:46:10 +0300

    ДЗ

--- Урок_6/task_1.py
+++ Урок_6/task_1.py
@@ -6,17 +6,24 @@
 from timeit import timeit
 from Урок_2.base_task_2 import switch
 
-from numpy import array
+import numpy as np
 
 def improved_switch(countries):
+    c = countries
     for index, value in enumerate(countries):
         if index % 2:
             countries[index], countries[index - 1] = countries[index - 1], countries[index]
     return countries
 
-# switch_mem([i for i in range(100000)])
-# improved_switch(array([i for i in range(10000)]))
+#print(switch([i for i in range(100000)]))
+#print(improved_switch(array([i for i in range(10000)])))
 
 
-print(timeit('switch([i for i in range(100000)])', setup='from __main__ import switch', number=1000))
+#print(timeit('switch([i for i in range(100000)])', setup='from __main__ import switch', number=1000))
+# 24.00459263997618
 
+print(timeit('improved_switch([i for i in range(100000)])', number=1000, globals=globals()))
+# 25.81123218301218
+a=np.array([i for i in range(100000)])
+print(a)
+print(improved_switch(a))
