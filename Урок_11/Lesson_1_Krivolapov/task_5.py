7a9dcec0e02bd42c05bdb69e96a1310dc65551d8
anton krivolapov <akrivola@ya.ru>
2023-01-18 11:46:57 +0300

    ДЗ урок 11

--- Урок_11/Lesson_1_Krivolapov/task_5.py
+++ Урок_11/Lesson_1_Krivolapov/task_5.py
@@ -7,3 +7,13 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+import subprocess
+import chardet
+
+ping_resource = [['ping', 'yandex.ru'], ['ping', 'youtube.com']]
+for ping in ping_resource:
+    ping_process = subprocess.Popen(ping, stdout=subprocess.PIPE)
+    for line in ping_process.stdout:
+        result = chardet.detect(line)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
